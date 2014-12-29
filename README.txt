d5cc0a216eb612b3240a13469acfedf18a5d3e7a
adrianarroyoceja <shadow_red_link@hotmail.com>
2014-12-29 13:20:54 -0600

    Initial commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+This is a project in C# for the Orexis Corp
+
+This Library class contains the generic clases and functions for all the software suite.
+It contain or will contain the DataBase utilities for login users, permision users, connectios to schemas and querys executions, logs reports for Oracle and Firebird Databases.
+
+Made by Adrian Arroyo Ceja
---
+++ Utilities/DataBAse.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//using System.Data.OracleClient;
+using Oracle.DataAccess.Client;
+using FirebirdSql.Data.FirebirdClient;
+
+
+namespace Utilities
+{
+    public class DataBase
+    {
+        public static OracleConnection connectOracle(string ip, string service, string user, string pass)
+        {
+            try
+            {
+                string connectionString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=" + ip + ")(PORT=1521))(CONNECT_DATA=(SERVICE_NAME="
+                    + service + ")));User Id=" + user + ";Password=" + pass;
+                OracleConnection remoteConnection = new OracleConnection(connectionString);
+                remoteConnection.Open();
+                return remoteConnection;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                return null;
+            }
+        }
+
+        public static void closeOracle(OracleConnection connection) //OracleDataReader dr //Si abrieramos 
+        {
+            try
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                    OracleConnection.ClearPool(connection);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        public static FbConnection connectFirebird(string user, string pass, string database, string datasource)
+        {
+            try
+            {
+                string connectionString = @"user=" + user + "; pass=" + pass + "; database=" + database + "; datasource=" + datasource + ";";
+                FbConnection remoteConnection = new FbConnection(connectionString);
+                remoteConnection.Open();
+                return remoteConnection;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                return null;
+            }
+        }
+
+        public static void closeFirebird(FbConnection connection) //Pudiera ir reader
+        {
+            try
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                    FbConnection.ClearPool(connection);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+    }
+}
